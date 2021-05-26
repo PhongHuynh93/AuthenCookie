4a73a00b9e0211e0e67ceb676a91cd5264eaa76d
Do Nguyen <donguyen@kryptowire.com>
2021-05-26 23:20:19 +0700

    Remove unused class

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { User } from './user';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 
