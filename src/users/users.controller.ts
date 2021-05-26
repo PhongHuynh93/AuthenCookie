d711c1ffc37ca51c1b3b40f3ecfc7a82c09d73bc
Do Nguyen <donguyen@kryptowire.com>
2021-05-26 23:18:41 +0700

    Add register user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegistrationReqDto } from '../dto/registration.req.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private userService: UsersService) { }
+
+    @Post('registration')
+    registerUser(@Body(ValidationPipe) reg: RegistrationReqDto) {
+        return this.userService.registerUser(reg);
+    }
+}
